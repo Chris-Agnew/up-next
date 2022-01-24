0ae65491c21b80e24b6082027ed4c76c2a272903
Chris <chris@chris-agnew.com>
2022-01-23 22:00:26 -0500

    initial set up

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kanit: ["Kanit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
